6b91299b85a4d5304a64f791143631aa55565a05
Saito-omochi <saito3cat@gmail.com>
2024-02-16 18:56:06 +0900

    08-3-4commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,15 +9,17 @@
     <body>
         <h1>Blog Name</h1>
         <div class = 'posts'>
-            @foreach($disp as $p)
+            @foreach($posts as $p)
                 <div class='post'>
-                    <h2 class='title'>{{$p -> title}}</h2>
+                    <h2 class='title'>
+                        <a href="/posts/{{$p -> id}}">{{$p -> title}}</a>
+                    </h2>
                     <p class = 'body'>{{$p -> body}}</p>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>
-            {{$disp -> links()}}
+            {{$posts -> links()}}
         </div>
     </body>
 </html>
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,23 @@
+<!DOCTIPE html>
+<html lang = "ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts-->
+        <link href = "https://fonts.googleapis.com/css?family=Nunito:200,600" rel = "stylrsheet">
+    </head>
+    <body>
+        <div class = "title">
+            <h1>{{$post -> title}}</h1>
+        </div>
+        <div class = "content">
+            <div class="content__post">
+                <h3>本文</h3>
+                <p>{{$post -> body}}</p>
+            </div>
+        </div>
+        <div class = "fotter">
+            <a href = '/'>戻る</a>
+        </div>
+    </body>
+</html>
